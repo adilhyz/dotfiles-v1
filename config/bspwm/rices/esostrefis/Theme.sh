c861993d91e8b8bff9f0571e5c5bc9206c5a5443
adilhyz <sandyadilhayyi@gmail.com>
2023-12-23 14:35:08 +0700

    Little change

--- config/bspwm/rices/esostrefis/Theme.sh
+++ config/bspwm/rices/esostrefis/Theme.sh
@@ -38,8 +38,8 @@ set_term_config() {
 set_picom_config() {
 	sed -i "$HOME"/.config/bspwm/picom.conf \
 		-e "s/normal = .*/normal =  { fade = true; shadow = true; }/g" \
-		-e "s/inactive-opacity = .*/inactive-opacity = 1;/g" \
-		-e "s/active-opacity = .*/active-opacity = 1;/g" \
+		-e "s/inactive-opacity = .*/inactive-opacity = 1.0;/g" \
+		-e "s/active-opacity = .*/active-opacity = 1.0;/g" \
 		-e "s/shadow-color = .*/shadow-color = \"#000000\"/g" \
 		-e "s/method = .*/method = \"none\"/g" \
 		-e "s/background = .*/background = false/g" \
